bf8e57bc521947a204e51c21bbe858c4dfc86783
IlyasKhoirulA <123036617+IlyasKhoirulA@users.noreply.github.com>
2023-03-28 23:05:42 +0700

    tambah view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css" />
+	<title>PAC IPNU IPPNU CIKALONGWETAN</title>
+</head>
+<body>
+	<div class="fcontainer">
+		<nav class="wrapper">
+			<div class="brand">
+				<div><img src="ipnu.png" alt="..."></div>
+				<div class="first">PAC IPNU CIKALONGWETAN</div>
+			</div>
+			<ul class="navigation">
+				<li><a href="">Beranda</a></li>
+				<li><a href="">Kontak</a></li>
+				<li><a href="">Tentang</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div>
+		<footer>
+			<p>Hak Cipta@2023 PAC IPNU Cikalongwetan</p>
+		</footer>
+	</div>
+</body>
+</html>
