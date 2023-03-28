ec63995da67cb74884435717bed0b653d7ca74a3
IlyasKhoirulA <123036617+IlyasKhoirulA@users.noreply.github.com>
2023-03-29 00:24:03 +0700

    halaman depan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css" />
-	<title>PAC IPNU IPPNU CIKALONGWETAN</title>
+	<link rel="stylesheet" href="css/style.css" />
+	<title>PELAJAR NU CIKALONGWETAN</title>
 </head>
 <body>
-	<div class="fcontainer">
+
+	{{-- bagian header --}}
+	<header class="fcontainer">
 		<nav class="wrapper">
 			<div class="brand">
-				<div><img src="ipnu.png" alt="..."></div>
-				<div class="first">PAC IPNU CIKALONGWETAN</div>
+				<div><img src="img/ipnu.png" alt="..."></div>
+				<div class="first">PELAJAR NU CIKALONGWETAN</div>
 			</div>
 			<ul class="navigation">
 				<li><a href="">Beranda</a></li>
@@ -20,11 +22,18 @@
 				<li><a href="">Tentang</a></li>
 			</ul>
 		</nav>
-	</div>
-	<div>
-		<footer>
-			<p>Hak Cipta@2023 PAC IPNU Cikalongwetan</p>
-		</footer>
-	</div>
+	</header>
+
+	{{-- bagian content --}}
+	<section>
+		<h1>test</h1>
+	</section>
+
+
+	{{-- bagian penutup --}}
+	<footer>
+			<p>Hak Cipta@2023 Pelajar NU Cikalongwetan</p>
+	</footer>
+
 </body>
 </html>
